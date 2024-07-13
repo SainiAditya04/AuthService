d53c6d6fbcf674db08dfdfc80bc580e7ca931b46
Aditya Saini <adityasaini@Adityas-MacBook-Air-3.local>
2024-07-13 22:25:46 +0530

    added Roles and User_Roles with the help of through tables

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const app = express();
+const apiRoutes = require("./routes/index");
 require("dotenv").config();
 
-const apiRoutes = require("./routes/index");
+const { User, Role } = require("./models/index");
+
+const db = require("./models/index");
 
 const PORT = process.env.PORT;
 
-const startServer = () => {
+const startServer = async () => {
 
     app.use(express.json());
     app.use("/api", apiRoutes);
+
+    // if(process.env.DB_SYNC){
+    //     db.sequelize.sync({ alter: true });
+    // }
+
+    const u1 = await User.findByPk(4);
+    const r1 = await Role.findByPk(1);
+    u1.addRole(r1);
 
     app.listen(PORT, async (req, res) => {
         console.log(`The server is started at PORT: ${PORT}`);
